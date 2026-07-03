45d7037da8276c7eef3700108e459a048452d94a
webdevred <148627186+webdevred@users.noreply.github.com>
2026-07-03 06:15:11 +0200

    Only create new release when relevant files change

--- .github/scripts/create_head_release.sh
+++ .github/scripts/create_head_release.sh
@@ -7,7 +7,8 @@ git fetch
 
 files_changed=$(git --no-pager diff --name-only origin/$TAG)
 if [[ $? -eq 0 ]] && ! echo "$files_changed" | grep -qE '^(action\.yml|setup-get-tested/action\.yml)$'; then
-  echo "No relevant files changed, skipping tag update."
+    echo "No relevant files changed, skipping tag update."
+    echo "new_release_needed=false" | tee "$GITHUB_OUTPUT"
   exit 0
 fi
 
@@ -15,3 +16,5 @@ gh release delete "$TAG" --yes --cleanup-tag || true
 git push origin ":refs/tags/$TAG" 2>/dev/null || true
 git tag "$TAG"
 git push -f origin "$TAG"
+
+echo "new_release_needed=true" | tee "$GITHUB_OUTPUT"
